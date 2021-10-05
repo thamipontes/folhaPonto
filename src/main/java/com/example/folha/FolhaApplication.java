bc609b66e0bd928f73419391f5987a65a619d547
Thamires <thamiresdepontesdasilva@gmail.com>
2021-10-05 09:22:23 -0300

    password cripotgrafia

--- src/main/java/com/example/folha/FolhaApplication.java
+++ src/main/java/com/example/folha/FolhaApplication.java
@@ -2,12 +2,13 @@ package com.example.folha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FolhaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FolhaApplication.class, args);
+        System.out.print(new BCryptPasswordEncoder().encode("123"));
     }
-
 }
