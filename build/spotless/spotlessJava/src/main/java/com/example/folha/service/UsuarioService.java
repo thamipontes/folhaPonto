b5bd23df1c4d03741b18a75dd2602c453c59af28
Thamires <thamiresdepontesdasilva@gmail.com>
2021-12-10 10:19:26 -0300

    Teste

---
+++ build/spotless/spotlessJava/src/main/java/com/example/folha/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package com.example.folha.service;
+
+import com.example.folha.dto.UsuarioDTO;
+import com.example.folha.entity.Role;
+import com.example.folha.entity.Usuario;
+import com.example.folha.repository.UsuariosRepository;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private final UsuariosRepository usuariosRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UsuarioService(UsuariosRepository usuariosRepository) {
+        this.usuariosRepository = usuariosRepository;
+    }
+
+    public Usuario salvarUsuario(UsuarioDTO usuarioDTO) {
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
+        usuarioDTO.setDataCriacao(LocalDateTime.now());
+        usuarioDTO.setRole(Role.ROLE_USER);
+
+        Usuario usuario = new Usuario();
+        BeanUtils.copyProperties(usuarioDTO, usuario);
+        return usuariosRepository.save(usuario);
+    }
+
+    public Usuario mudarRole(Role novaRole, String login) {
+        Usuario usuario = Optional
+            .ofNullable(usuariosRepository.findByLogin(login))
+            .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+        usuario.setRole(novaRole);
+        return usuariosRepository.save(usuario);
+    }
+}
