3f073e1754db4b73a38f9004971d3074fd654886
Thamires <thamiresdepontesdasilva@gmail.com>
2021-12-19 16:03:31 -0300

    Eureka

--- src/main/java/com/example/folha/FolhaApplication.java
+++ src/main/java/com/example/folha/FolhaApplication.java
@@ -3,9 +3,11 @@ package com.example.folha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class FolhaApplication {
 
     public static void main(String[] args) {
